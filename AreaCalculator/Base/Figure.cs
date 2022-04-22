48a79d0cdfb6918f13195458150f441dc47e5bf4
kaye-la <61543425+kaye-la@users.noreply.github.com>
2022-04-22 19:54:23 +0300

    Add Figure and Circle

---
+++ AreaCalculator/Base/Figure.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator.Base
+{
+    public abstract class Figure
+    {
+        private readonly Lazy<double> _area;
+
+        protected Figure()
+        {
+            _area = new Lazy<double>(CalculateArea);
+        }
+
+        /// <summary>
+        /// Property to get Figure's area
+        /// </summary>
+        public double Area => _area.Value;
+
+        /// <summary>
+        /// Method to calculate Figure's area
+        /// </summary>
+        /// <returns>Figure's area</returns>
+        protected abstract double CalculateArea();
+
+        /// <summary>
+        /// Method to validate anything
+        /// </summary>
+        /// <returns>True - if validation succeed. False - if not</returns>
+        protected abstract bool Validate();
+    }
+}
---
+++ AreaCalculator/Figures/Circle.cs
@@ -0,0 +1,34 @@
+﻿using AreaCalculator.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator.Figures
+{
+    public class Circle : Figure
+    {
+        private readonly double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+            Validate();
+        }
+
+        protected override double CalculateArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+
+        protected override bool Validate()
+        {
+            if (_radius < 0)
+                throw new ArgumentException("Argument can't be negative");
+            else if (_radius == 0)
+                throw new ArgumentException("Argument can't be negative");
+            return true;
+        }
+    }
+}
